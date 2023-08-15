8efa14aa4d7082514bf1b66c3e2cae152e1207eb
Indrajit-Das <indrojit210210@gmail.com>
2023-08-15 13:23:05 +0600

    adding progress bar and active inactive features in app

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 // progress bar next-np-progressbar
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Blog project for assignment-11',
