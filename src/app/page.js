b1ccdd5e8de58201969abcf5cba7e347abc981e7
Indrajit-Das <indrojit210210@gmail.com>
2023-08-15 21:04:21 +0600

    change in home page message

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import styles from "./Home.module.css"
 export default function Home() {
   return (
-      <h1 className={styles.text}>Welcome to home page</h1>
+      <h1 className={styles.text}>Welcome to Blog Home Page</h1>
   )
 }
